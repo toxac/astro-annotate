5549adf85f9dc15198a070adad9ebfd9aba846d3
toxac <derocon@gmail.com>
2025-03-18 16:10:55 +0530

    folder structure

---
+++ src/api/annotate.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+interface Annotation {
+  highlightId: string;
+  text: string;
+  comment: string;
+  pageUrl: string;
+}
+
+const dataPath = path.resolve(__dirname, 'annotations.json');
+
+export function saveAnnotation(annotation: Annotation) {
+  let annotations: Annotation[] = [];
+  if (fs.existsSync(dataPath)) {
+    annotations = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+  }
+  annotations.push(annotation);
+  fs.writeFileSync(dataPath, JSON.stringify(annotations, null, 2));
+}
+
+export function getAnnotations(pageUrl: string): Annotation[] {
+  if (!fs.existsSync(dataPath)) return [];
+  const annotations = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+  return annotations.filter((a: Annotation) => a.pageUrl === pageUrl);
+}
